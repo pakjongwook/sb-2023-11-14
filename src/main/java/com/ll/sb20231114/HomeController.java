fa2311bf5d2ed854d1d5dc37a509cfc7baeab5f2
pakjongwook <ejck9597@gmail.com>
2023-11-14 11:03:47 +0900

    calc

--- src/main/java/com/ll/sb20231114/HomeController.java
+++ src/main/java/com/ll/sb20231114/HomeController.java
@@ -2,6 +2,7 @@ package com.ll.sb20231114;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller // 어노테이션 == 주석
@@ -16,5 +17,42 @@ public class HomeController { // Controller : 은행 창고 직원 역할
     @ResponseBody
     String showAbout(){
         return "개발자 커뮤니티";
+    }
+
+    @GetMapping("/calc") //http://localhost:8020/calc?a=10&b=20
+    @ResponseBody
+    String showCalc(int a, int b){ // 안됨 : int(primitive type) : null 허용 X
+        return "계산 결과 : %d".formatted(a + b);
+    }
+    @GetMapping("/calc2") // http://localhost:8020/calc2?a=10&b=20
+    @ResponseBody
+    String showCalc2(Integer a, Integer b){ // Integer : null 허용됨
+        return "a :" + a + "b :" + b;
+    }
+    @GetMapping("/calc3") // http://localhost:8020/calc3?a=10&b=20
+    @ResponseBody
+    String showCalc3(
+            @RequestParam(defaultValue = "0") int a, 
+            // 클라이언트가 전달하는 쿼리 파라미터를 서버에서 처리할 수 있게 해주는 어노테이션
+            @RequestParam(defaultValue = "0") int b
+    ) {
+        return "계산 결과 : %d".formatted(a + b);
+    }
+    @GetMapping("/calc4")
+    @ResponseBody
+    String showCalc4(
+            @RequestParam(defaultValue = "0") double a,
+            @RequestParam(defaultValue = "0") double b
+    ) {
+        return "계산 결과 : %f".formatted(a + b);
+    }
+
+    @GetMapping("/calc5")
+    @ResponseBody
+    String showCalc4(
+            @RequestParam(defaultValue = "-") String a,
+            @RequestParam(defaultValue = "-") String b
+    ) {
+        return "계산 결과 : %s".formatted(a + b);
     }
 }
